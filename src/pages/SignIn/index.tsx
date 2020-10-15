de10e3ce77ce2f4dec4dcef10def6c997b717706
Victor Mesquita <mesquitadev@gmail.com>
2020-10-15 16:51:13 -0300

    M - SignIn - Criando Pagina de Login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import {Image} from 'react-native';
-import {Container} from './styles';
+import {Image, KeyboardAvoidingView, Platform, ScrollView} from 'react-native';
+import {Container, styles} from './styles';
+import {Input, Button} from '../../components';
+import {PrimaryText} from '../../styles';
+
 import logo from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logo} />
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{flex: 1}}>
+          <Image style={styles.logo} source={logo} width={156} height={40} />
+          <Container>
+            <PrimaryText fontSize={14} alignSelf="flex-start">
+              E-mail
+            </PrimaryText>
+            <Input name="email" placeholder="E-mail" />
+            <PrimaryText fontSize={14} alignSelf="flex-start">
+              Senha
+            </PrimaryText>
+            <Input name="password" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('oka');
+              }}>
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components/native';
+import {StyleSheet} from 'react-native';
+export const Container = styled.View`
+  margin-top: 78px;
+  align-items: center;
+  justify-content: center;
+  padding: 0 16px;
+`;
 
-export const Container = styled.View``;
+export const styles = StyleSheet.create({
+  logo: {
+    alignSelf: 'center',
+    marginTop: 106,
+  },
+});
